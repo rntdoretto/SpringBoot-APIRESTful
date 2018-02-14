1dcb379baf2f1a9b2ce0d02a0bcb84426a057212
Renato Doretto <renato.doretto@outlook.com>
2018-02-13 22:05:10 -0200

    Ajustada integração e versionamento com Flyway.

--- src/main/resources/db/migration/mysql/V2__admin_padrao.sql
+++ src/main/resources/db/migration/mysql/V2__admin_padrao.sql
@@ -1,39 +1,39 @@
-INSERT INTO empresa
-            (id,
-             cnpj,
-             data_atualizacao,
-             data_criacao,
-             razao_social)
-VALUES      (NULL,
-             '61372084000189',
-             CURRENT_DATE(),
-             CURRENT_DATE(),
-             'Doretto TI');
+INSERT INTO empresa 
+            (id, 
+             cnpj, 
+             data_atualizacao, 
+             data_criacao, 
+             razao_social) 
+VALUES      (NULL, 
+             '61372084000189', 
+             CURRENT_DATE(), 
+             CURRENT_DATE(), 
+             'Doretto TI'); 
 
-INSERT INTO funcionario
-            (id,
-             cpf,
-             data_atualizacao,
-             data_criacao,
-             email,
-             nome,
-             perfil,
-             qtd_horas_almoco,
-             qtd_horas_trabalho_dia,
-             senha,
-             valor_hora,
-             empresa_id)
-VALUES      (NULL,
-             '30788714015',
-             CURRENT_DATE(),
-             CURRENT_DATE(),
-             'admin@doretto.com',
-             'ADMIN',
-             'ROLE_ADMIN',
-             NULL,
-             NULL,
-             '$2a$10$3JngdAbTTUN1P9do.CSMWuqOMLqzfXgdN0N7U/OACNLLY3YQrCE4O',
-             NULL,
-             (SELECT id
-              FROM   empresa
+INSERT INTO funcionario 
+            (id, 
+             cpf, 
+             data_atualizacao, 
+             data_criacao, 
+             email, 
+             nome, 
+             perfil, 
+             qtd_horas_almoco, 
+             qtd_horas_trabalho_dia, 
+             senha, 
+             valor_hora, 
+             empresa_id) 
+VALUES      (NULL, 
+             '30788714015', 
+             CURRENT_DATE(), 
+             CURRENT_DATE(), 
+             'admin@doretto.com', 
+             'ADMIN', 
+             'ROLE_ADMIN', 
+             NULL, 
+             NULL, 
+             '$2a$10$3JngdAbTTUN1P9do.CSMWuqOMLqzfXgdN0N7U/OACNLLY3YQrCE4O', 
+             NULL, 
+             (SELECT id 
+              FROM   empresa 
               WHERE  cnpj = '61372084000189')); 
